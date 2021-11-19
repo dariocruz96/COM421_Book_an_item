3da2f2e763bcd92a735b88b0347c7109e2a3dcc8
dariocruz96 <dariocruz96@hotmail.com>
2021-11-19 10:04:19 +0000

    Added option 3, A member of staff must be able to display all items booked for repair, sorted by customer name.

--- main.py
+++ main.py
@@ -29,6 +29,18 @@ class ListOfItems:
             else:
                 print("This customer name doesn't exist.")
 
+    def sortItem(self):
+
+        for i in range(0, len(self.listOfItems) - 1):
+            for j in range(0, len(self.listOfItems) - i - 1):
+                for listItem in self.listOfItems:
+                    for compareItem in self.listOfItems:
+                        if listItem.customerName > compareItem.customerName:
+                            tempVar = self.listOfItems[j]
+                            self.listOfItems[j] = self.listOfItems[j + 1]
+                            self.listOfItems[j + 1] = tempVar
+        return "\n".join(str(item) for item in self.listOfItems)
+
     # Returns readable content
     def __str__(self):
         return "\n".join(str(item) for item in self.listOfItems)
@@ -41,6 +53,7 @@ def menu():
     while True:
         print("\n1 - Book item")
         print("2 - Search item")
+        print("3 - Display all items booked for repair, sorted by customer name")
         print("x - Exit Application\n")
         choice = input()
         if choice:
@@ -49,16 +62,20 @@ def menu():
                 item1 = Item("Tiago", "Macbook", "Macbook pro 15.1 2016")
                 item2 = Item("Andre", "Laptop", "Asus ROG gtx 1060")
                 item3 = Item("Dario", "Desktop", "MSI GLA Combat")
+                # Adds items to our list
                 listOfItems.addItem(item1)
                 listOfItems.addItem(item2)
                 listOfItems.addItem(item3)
-                print(listOfItems)
+                print("Item added successfully!")
 
             # Search an item
             elif choice == "2":
                 foundItem = listOfItems.searchItem("Dario")
                 print("Found Item:")
                 print(foundItem)
+
+            elif choice == "3":
+                print(listOfItems.sortItem())
 
             # Exit program
             elif choice == "x":
