ec4dad7a85f3b44591193021553724e4910ac20b
dariocruz96 <dariocruz96@hotmail.com>
2021-11-26 10:56:54 +0000

    Added serial number to class Added serial number to every constructor Added search item by serial number Fixed output table design Added input from console

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 class Item:
     # Creates an item structure with costumer name, item type and item description
-    def __init__(self, customerName, itemType, itemDescription):
+    def __init__(self, customerName, itemType, itemDescription, serialNumber):
         self.customerName = customerName
         self.itemType = itemType
         self.itemDescription = itemDescription
+        self.serialNumber = serialNumber
+
 
     # Returns readable content
     def __str__(self):
-        return f"{self.customerName} {self.itemType} {self.itemDescription}"
+        return f"{self.customerName} {self.itemType} {self.itemDescription} {self.serialNumber}"
 
 
 class ListOfItems:
@@ -19,25 +21,36 @@ class ListOfItems:
     def addItem(self, item):
         self.listOfItems.append(item)
 
-    def popItem(self, customerName):
+    def popItem(self, serialNumber):
         for element in self.listOfItems:
-            # Looks for a entry with the same costumer name
-            if element.customerName == customerName:
+            # Looks for a entry with the same serial number
+            if int(element.serialNumber) == serialNumber:
                 self.listOfItems.remove(element)
                 return element
 
     # Searches for an item using costumer name
-    def searchItem(self, customerName):
+    def searchItemByName(self, customerName):
         temp = []
         # Looks for every entry in our list
         for element in self.listOfItems:
             # Looks for a entry with the same costumer name
-            if element.customerName == customerName:
-                temp.append((element.customerName, element.itemType, element.itemDescription))
+            if element.customerName.lower() == customerName.lower():
+                temp.append((element.customerName, element.itemType, element.itemDescription, element.serialNumber))
         if len(temp) == 0:
             print("This customer name doesn't exist.")
         return temp
 
+    # Searches for an item using costumer name
+    def searchItemBySerial(self, serialNumber):
+        temp = []
+        # Looks for every entry in our list
+        for element in self.listOfItems:
+            # Looks for a entry with the same costumer name
+            if element.serialNumber == serialNumber:
+                temp.append((element.customerName, element.itemType, element.itemDescription, element.serialNumber))
+        if len(temp) == 0:
+            print("This serial number doesn't exist.")
+        return temp
 
     # Displays all items booked for repair, sorted by customer name
     def sortItem(self):
@@ -52,32 +65,60 @@ class ListOfItems:
                     self.listOfItems[j + 1] = tempVar
         return "\n".join(str(item) for item in self.listOfItems)
 
+    # Calculate Hash
+    def serialNumber(self, name):
+        count = 0
+        hashCode = 0
+        for ch in name:
+            hashCode += (ord(ch) - 96) * (31 ** count)
+            count += 1
+        while True:
+            temp = 0
+            for x in self.listOfItems:
+                if x.serialNumber == str(hashCode):
+                    hashCode += 1
+                else:
+                    temp += 1
+            if temp == len(self.listOfItems):
+                break
 
+        return hashCode % 200
+
+    def tableDesign(self):
+        cn = "Customer Name"
+        it = "Item Type"
+        ides = "Item Description"
+        sc = "Serial Code"
+        bar = "|"
+        for j in range(222):
+            print("-", end="")
+        print("")
+        print(f"|{cn:>22}{it:>50}{ides:>91}{bar:>38}\t {sc:<16}|")
+        for i in self.listOfItems:
+            for j in range(222):
+                print("-", end="")
+            print("")
+            print(f"|\t{i.customerName:<35}|\t{i.itemType:<50}|\t{i.itemDescription:<105}|\t{i.serialNumber:<17}|")
+        for j in range(222):
+            print("-", end="")
 
     # Returns readable content
     def __str__(self):
         return "\n".join(str(item) for item in self.listOfItems)
 
-def tableDesign(my_list):
-    for i in my_list:
-        for j in range(101):
-            print("-", end="")
-        print("")
-        print(f"|\t{i.customerName:<25}|\t{i.itemType:<25}|\t{i.itemDescription:<40}|")
-    for j in range(101):
-        print("-", end="")
 
 def menu():
-    print("Welcome to Solent Computer Rapairs!\n"
+    print("Welcome to Solent Computer Repairs!\n"
           "How can I help you today?")
     # Creates our list
     listOfItems = ListOfItems()
+    firstAdd = 0
     # Runs the menu until user presses "x" and exits the program
     while True:
         print("\n1 - Book item")
-        print("2 - Search item")
+        print("2 - Search item by serial number")
         print("3 - Sort by customer name")
-        print("4 - Search item by costumer name")
+        print("4 - Search item by customer name")
         print("5 - Delete item")
 
         print("x - Exit Application\n")
@@ -85,45 +126,73 @@ def menu():
         if choice:
             # Book an item
             if choice == "1":
-                item1 = Item("Tiago", "Macbook", "Macbook pro 15.1 2016")
-                item2 = Item("Andre", "Laptop", "Asus ROG gtx 1060")
-                item3 = Item("Dario", "Desktop", "MSI GLA Combat")
-                item4 = Item("Filipe", "Laptop", "Alienware")
-                item5 = Item("Dario", "Desktop", "Rog MXO")
-                # Adds items to our list
-                listOfItems.addItem(item1)
-                listOfItems.addItem(item2)
-                listOfItems.addItem(item3)
-                listOfItems.addItem(item4)
-                listOfItems.addItem(item5)
-                print("Item added successfully!")
+                if firstAdd == 0:
+                    item1 = Item("Tiago", "Macbook", "Macbook pro 15.1 2016", "12341")
+                    item2 = Item("Andre", "Laptop", "Asus ROG gtx 1060", "123124")
+                    item3 = Item("Dario", "Desktop", "MSI GLA Combat", "123123")
+                    item4 = Item("Filipe", "Laptop", "Alienware", "12313")
+                    item5 = Item("Dario", "Desktop", "Rog MXO", "12412313")
+                    # Adds items to our list
+                    listOfItems.addItem(item1)
+                    listOfItems.addItem(item2)
+                    listOfItems.addItem(item3)
+                    listOfItems.addItem(item4)
+                    listOfItems.addItem(item5)
+                    firstAdd += 1
+                    print("BOOKING AN ITEM\n\n---Information Required---\n")
+                while True:
+                    customer_name = input("Customer name:\n")
+                    if customer_name == "":
+                        print("Please provide a customer name!")
+                    else:
+                        break
+                serial = listOfItems.serialNumber(customer_name)
+                item_type = input("Item type:\n")
+                item_description = input("Item description:\n")
+                item_temp = Item(f"{customer_name}", f"{item_type}", f"{item_description}", f"{serial}")
+                listOfItems.addItem(item_temp)
 
-            # Search an item
+                print(f"\n\nItem added successfully!\t --- SERIAL NUMBER : {serial} ---")
+
+            # Search an item by serial number
             elif choice == "2":
-                foundItem = listOfItems.searchItem("Dario")
+                tempList = ListOfItems()
+                serialNumber = input("Please provide the serial number associated to the item belongs:\n")
+                foundItem = listOfItems.searchItemBySerial(serialNumber)
                 print("Found Item:")
-                for costumer in foundItem:
-                    print(f"{costumer[0]} {costumer[1]} {costumer[2]}")
+                for customer in foundItem:
+                    item = Item(f"{customer[0]}", f"{customer[1]}", f"{customer[2]}", f"{customer[3]}")
+                    tempList.addItem(item)
+                tempList.tableDesign()
 
             # Display all items booked for repair, sorted by customer name
             elif choice == "3":
-                print(listOfItems.sortItem())
+                listOfItems.sortItem()
+                listOfItems.tableDesign()
 
             # Search an item by costumer name
             elif choice == "4":
-                foundItem = listOfItems.searchItem("Dario")
+                tempList = ListOfItems()
+                customer_name = input("Please provide the customer name:\n")
+                foundItem = listOfItems.searchItemByName(customer_name)
                 print("Found Item:")
-                for costumer in foundItem:
-                    print(f"{costumer[0]} {costumer[1]} {costumer[2]}")
+                for customer in foundItem:
+                    item = Item(f"{customer[0]}", f"{customer[1]}", f"{customer[2]}", f"{customer[3]}")
+                    tempList.addItem(item)
+                tempList.tableDesign()
 
             # Delete an item after being repaired
             elif choice == "5":
-                print("After repairing:")
-                print(listOfItems.popItem("Dario"))
-                print("The item will now be deleted from database...\nDone!")
+                tempList = ListOfItems()
+                print("Item already repaired:")
+                serial = int(input("Serial Number: "))
+                item = listOfItems.popItem(serial)
+                tempList.addItem(item)
+                tempList.tableDesign()
+                print("\nThe item will now be deleted from database...\nDone!")
 
             elif choice == "6":
-                tableDesign(listOfItems)
+                listOfItems.tableDesign()
 
             # Exit program
             elif choice == "x":
